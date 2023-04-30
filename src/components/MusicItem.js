b44b0d2012af7d3207340ff5a9bbf5a8c1bd1271
jjf5pf <jjf5pf@virginia.edu>
2023-04-30 15:25:32 -0400

    pleasepleaseplease

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -1,11 +1,60 @@
 import missing from "../images/missing.jpeg"
+import Popup from "reactjs-popup";
 
 export default function MusicItem(props) {
     return (
-        <div className="MusicItem">
-            <img className="thumbnail" src={props.Thumbnail === "" ? missing : props.Thumbnail}></img>
-            <p className="Title">{props.Name}</p>
-            <p className="Artist">{props.Artist}</p>
+        <Popup
+            trigger={<button className="button-music"> <div className="MusicItem">
+                <img className="thumbnail" src={props.Thumbnail === "" ? missing : props.Thumbnail}></img>
+                <p className="Title">{props.Name}</p>
+                <p className="Artist">{props.Artist}</p>
+        </div> </button>}
+            modal
+            nested
+        >
+        {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="content">
+            <img className="album" src={props.Thumbnail === "" ? missing : props.Thumbnail}></img>
+            {' '}
+            {props.Artist}
+            <br />
+            {props.CDtype}
         </div>
+        <div className="actions">
+          <Popup
+            trigger={<button className="button"> Trigger </button>}
+            position="top center"
+            nested
+          >
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+              magni omnis delectus nemo, maxime molestiae dolorem numquam
+              mollitia, voluptate ea, accusamus excepturi deleniti ratione
+              sapiente! Laudantium, aperiam doloribus. Odit, aut.
+            </span>
+          </Popup>
+          <button
+            className="button"
+            onClick={() => {
+              console.log('modal closed ');
+              close();
+            }}
+          >
+            close modal
+          </button>
+        </div>
+      </div>
+    )}
+    </Popup>
+    //     <div className="MusicItem">
+    //         <img className="thumbnail" src={props.Thumbnail === "" ? missing : props.Thumbnail}></img>
+    //         <p className="Title">{props.Name}</p>
+    //         <p className="Artist">{props.Artist}</p>
+    //     </div>
+    // );
     );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,15 @@
 import Navbar from "../components/NavBar";
 import MusicItem from "../components/MusicItem";
 import { useState, useEffect } from "react";
+import React from "react";
+import Popup from "reactjs-popup";
 
 export default function Home(props) {
     const [tradeData, setTradeData] = useState([])
     const [filter, setFilter] = useState("all")
 
     useEffect(() => {
-        fetch(`http://localhost:3000/${filter}`)
+        fetch(`http://localhost:8080/${filter}`)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
@@ -16,7 +18,7 @@ export default function Home(props) {
     }, [])
 
     useEffect(() => {
-        fetch(`http://localhost:3000/${filter}`)
+        fetch(`http://localhost:8080/${filter}`)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
@@ -24,12 +26,65 @@ export default function Home(props) {
             });
     }, [filter])
 
+
+    const PopupExample = () => (
+        <Popup
+            trigger={<button className="button"> Open Modal </button>}
+            modal
+            nested
+        >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Modal Title </div>
+        <div className="content">
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+        </div>
+        <div className="actions">
+          <Popup
+            trigger={<button className="button"> Trigger </button>}
+            position="top center"
+            nested
+          >
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+              magni omnis delectus nemo, maxime molestiae dolorem numquam
+              mollitia, voluptate ea, accusamus excepturi deleniti ratione
+              sapiente! Laudantium, aperiam doloribus. Odit, aut.
+            </span>
+          </Popup>
+          <button
+            className="button"
+            onClick={() => {
+              console.log('modal closed ');
+              close();
+            }}
+          >
+            close modal
+          </button>
+        </div>
+      </div>
+    )}
+    </Popup>
+    );
+
+
     const tradeElements = tradeData.map(trade => <MusicItem {...trade} />)
 
     return (
+
         <div>
             <Navbar logOut={props.logOut} {...props.profile}/>
-
+            <PopupExample/>
             <div className="home-feed">
                 <select className="filter"
                     value={filter} 
@@ -40,8 +95,11 @@ export default function Home(props) {
                     <option value="cassettes">Cassettes</option>
                 </select>
 
+                
+                <div id="popup-root" />
+
                 <div className="music-grid">
-                    {tradeElements}
+                    {tradeElements} 
                 </div>
             </div>
         </div>
