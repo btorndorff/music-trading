11de74b24ef7652212de638c7903a4a7c10492f1
Benjamin Orndorff <borff@Benjamins-MacBook-Pro.local>
2023-05-01 00:18:26 -0400

    aga

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,22 @@ import logo from '../images/logo.png'
 import add from '../images/add.png'
 import Popup from "reactjs-popup";
 import AddMusic from './AddMusic';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     // console.log(props)
     return (
         <nav className="nav">
             <div className='nav-buttons'>
-                <img className="logo" src={logo} alt="logo" />
+                <Link to="/" >
+                    <img className='logo' src={logo} alt="logo" />
+                </Link>
+                
                 <div class="nav-right">
+                    <Link style={{textDecoration: "none", color:'white'}} to={`/profile/${props.id}`}>
+                        <img className="profile-img" src={props.picture}/>
+                    </Link>
+
                     <Popup
                         trigger={<button className="button-music"> <div className="MusicItem">
                         <img className="add" src={add} alt="add" />
@@ -26,8 +34,7 @@ export default function Navbar(props) {
 
 
                     
-                    <button onClick={props.logOut}>Log out</button>
-                    <img />
+                    <button style={{width: "50%"}} onClick={props.logOut}>Log out</button>
                 </div>
 
             </div>
